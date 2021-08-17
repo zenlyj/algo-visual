430e0a31412311171a5600339c8564c193bc2a55
Zen Lee Yijie <iarezenly@gmail.com>
2021-08-18 05:06:18 +0800

    Enable pausing of sorting

--- src/Sorter.js
+++ src/Sorter.js
@@ -15,11 +15,17 @@ class Sorter extends React.Component {
             scanElement:null,
             pivotBefore:null,
             pivotAfter:null,
-            delay:100
+            delay:100,
+            buffer:null
         }
         this.setAlgo = this.setAlgo.bind(this)
         this.generateRandomArray = this.generateRandomArray.bind(this)
         this.start = this.start.bind(this)
+        this.pause = this.pause.bind(this)
+    }
+    
+    componentDidMount() {
+        this.generateRandomArray()
     }
 
     randInt = (low, high) => {
@@ -41,9 +47,9 @@ class Sorter extends React.Component {
     }
 
     async playbackSelection(createPromise) {
-        const buffer = selectionSort(this.state.array)
+        const buffer = this.state.buffer
         const sortedSet = new Set()
-        while (!buffer.isEmpty()) {
+        while (!buffer.isEmpty() && this.state.isRunning) {
             const scan = buffer.consumeScan()
             const diagram = buffer.consumeDiagram()
             for (let i = 0; i < scan.length; i++) {
@@ -55,9 +61,9 @@ class Sorter extends React.Component {
     }
 
     async playbackBubble(createPromise) {
-        const buffer = bubbleSort(this.state.array)
+        const buffer = this.state.buffer
         const sortedSet = new Set()
-        while (!buffer.isEmpty()) {
+        while (!buffer.isEmpty() && this.state.isRunning) {
             const scan = buffer.consumeScan()
             const diagram = buffer.consumeDiagram()
             if (scan === undefined && diagram === undefined) {
@@ -74,10 +80,10 @@ class Sorter extends React.Component {
     }
 
     async playbackMerge(createPromise) {
-        const buffer = mergeSort(this.state.array)
+        const buffer = this.state.buffer
         const sortedSet = new Set()
         let i = 0
-        while (!buffer.isEmpty()) {
+        while (!buffer.isEmpty() && this.state.isRunning) {
             i++
             if (i >= this.state.array.length) {
                 for (let j = 0; j < this.state.array.length; j++) sortedSet.add(buffer.consumeSorted())
@@ -94,9 +100,9 @@ class Sorter extends React.Component {
     }
 
     async playbackQuick(createPromise) {
-        const buffer = quickSort(this.state.array)
+        const buffer = this.state.buffer
         const sortedSet = new Set()
-        while (!buffer.isEmpty()) {
+        while (!buffer.isEmpty() && this.state.isRunning) {
             const diagram = buffer.consumeDiagram()
             const scanThru = buffer.consumeScan()
             const pivot = buffer.consumePivots()
@@ -112,9 +118,9 @@ class Sorter extends React.Component {
     }
 
     async playbackGravity(createPromise) {
-        const buffer = gravitySort(this.state.array)
+        const buffer = this.state.buffer
         const sortedSet = new Set()
-        while (!buffer.isEmpty()) {
+        while (!buffer.isEmpty() && this.state.isRunning) {
             const diagram = buffer.consumeDiagram()
             if (diagram === undefined) {
                 for (let i = 0; i < this.state.array.length; i++) sortedSet.add(buffer.consumeSorted())
@@ -125,9 +131,31 @@ class Sorter extends React.Component {
         }
     }
 
+    bufferAlgo() {
+        let buffer = null
+        switch(this.state.selectedAlgo) {
+            case 'SELECTION SORT':
+                buffer = selectionSort(this.state.array)
+                break
+            case 'BUBBLE SORT':
+                buffer = bubbleSort(this.state.array)
+                break
+            case 'MERGE SORT':
+                buffer = mergeSort(this.state.array)
+                break
+            case 'QUICK SORT':
+                buffer = quickSort(this.state.array)
+                break
+            case 'GRAVITY SORT':
+                buffer = gravitySort(this.state.array)
+                break
+            default:
+        }
+        this.setState({buffer:buffer})
+    }
+
     start() {
         const createPromise = (delay, array, sorted, scanElement, pivotBefore, pivotAfter) => {
-            console.log(pivotAfter)
             return new Promise((resolve) => {
                 setTimeout(() => {
                     this.setState({array:array, sorted:sorted, scanElement:scanElement, pivotBefore:pivotBefore, pivotAfter:pivotAfter})
@@ -135,30 +163,39 @@ class Sorter extends React.Component {
                 }, delay)
             })
         }
-        switch(this.state.selectedAlgo) {
-            case 'SELECTION SORT':
-                this.playbackSelection(createPromise)
-                break
-            case 'BUBBLE SORT':
-                this.playbackBubble(createPromise)
-                break
-            case 'MERGE SORT':
-                this.playbackMerge(createPromise)
-                break
-            case 'QUICK SORT':
-                this.playbackQuick(createPromise)
-                break
-            case 'GRAVITY SORT':
-                this.playbackGravity(createPromise)
-                break
-            default:
+        if (this.state.buffer === null) {
+            this.bufferAlgo()
         }
+        this.setState({isRunning:true}, ()=> {
+            switch(this.state.selectedAlgo) {
+                case 'SELECTION SORT':
+                    this.playbackSelection(createPromise)
+                    break
+                case 'BUBBLE SORT':
+                    this.playbackBubble(createPromise)
+                    break
+                case 'MERGE SORT':
+                    this.playbackMerge(createPromise)
+                    break
+                case 'QUICK SORT':
+                    this.playbackQuick(createPromise)
+                    break
+                case 'GRAVITY SORT':
+                    this.playbackGravity(createPromise)
+                    break
+                default:
+            }
+        })
+    }
+
+    pause() {
+        this.setState({isRunning:false})
     }
 
     render() {
         return (
             <div>
-                <SortingMenuBar selectedAlgo={this.state.selectedAlgo} setAlgo={this.setAlgo} generateRandomArray={this.generateRandomArray} start={this.start}/>
+                <SortingMenuBar isRunning={this.state.isRunning} selectedAlgo={this.state.selectedAlgo} setAlgo={this.setAlgo} generateRandomArray={this.generateRandomArray} start={this.start} pause={this.pause}/>
                 <SortChart array={this.state.array} sorted={this.state.sorted} scanElement={this.state.scanElement} pivotBefore={this.state.pivotBefore} pivotAfter={this.state.pivotAfter}/>
             </div>
         )
