35e1e3421e9cefa3dab2ce77e5ebd452cf0f2bb5
Zen Lee Yijie <iarezenly@gmail.com>
2021-08-23 00:03:23 +0800

    Add route to user guide

--- src/HomeHeader.js
+++ src/HomeHeader.js
@@ -7,7 +7,7 @@ class HomeHeader extends React.Component {
             <div className="header">
                 <a href="#0" onClick={()=>this.props.setHomePage()} className="logo">ALGORITHM VISUALIZER</a>
                 <div className="header-right">
-                    <a href="#0">HOW TO USE</a>
+                    <a href="https://zenlyj.github.io/algo-visualizer/" rel="noreferrer noopener" target="_blank">HOW TO USE</a>
                     <a href="https://github.com/zenlyj/algo-visualizer" rel="noreferrer noopener" target="_blank">VIEW ON GITHUB</a>
                 </div>
             </div>
